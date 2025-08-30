f66ca0a19a891aa95d5177635df493e88c686c96
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2025-08-29 21:23:35 -0600

    feat: Migrated persona-emails command from previous project

--- audit365/cli.py
+++ audit365/cli.py
@@ -1,11 +1,12 @@
 import click
 
+from audit365.commands.personal_emails import personal_emails
 
 @click.group()
 def cli() -> None:
     pass
 
-
+cli.add_command(personal_emails)
 
 if __name__ == '__main__':
     pass
